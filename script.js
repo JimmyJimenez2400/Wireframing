7250d15d7c719fa47fe5b8fe21c6e57182101b4a
Jason Orr <jason.orr94@gmail.com>
2021-01-24 23:34:29 -0500

    repsonsive changes sidebar

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 /*============= SIDEBAR ==========*/
 function openNav() {
+    if (screen.width > 680) {
     document.getElementById("sideNav").style.width = "250px";
     document.getElementById("mainPage").style.marginLeft = "250px";
     document.getElementById("menuBtn").style.display = "none";
+    } else {
+        document.getElementById("sideNav").style.width = "150px";
+        document.getElementById("mainPage").style.marginLeft = "150px";
+        document.getElementById("menuBtn").style.display = "none";
+    }
 }
 
 function closeNav() {
